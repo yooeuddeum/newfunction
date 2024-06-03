4fc805b38afe9481263d876ea00b7bca22d9af50
유으뜸 <www.goolge.com>
2024-06-03 11:52:24 +0900

    변경 완료

--- work1.js
+++ work1.js
@@ -2,19 +2,38 @@
 // * 목적은 함수를 조금 더 이해하려고 합니다.
 // * 매개변수가 객체인 경우가 아래에 작성될 예정입니다.
 
+// function workone(a, b) {
+// // 매개 변수 두개를 받은 다음
+// // return은 객체였으면 좋겠음
+//  let localVariable = {};
+//  localVariable[a] = a;
+//  localVariable[b] = b;
+// return localVariable;
+// };
+
+// let test = workone("유으뜸","euddeum");
+// console.log(test);
+
 function workone(a, b) {
-// 매개 변수 두개를 받은 다음
-// return은 객체였으면 좋겠음
- let localVariable = {};
- localVariable[a] = a;
- localVariable[b] = b;
-return localVariable;
-};
+  // 매개 변수 두개를 받은 다음
+  // return은 객체였으면 좋겠음
+  // 매개변수가 모두 문자열이여야 한다. 조건문 걸어줌
+  if (typeof a === "string") {
+    if (typeof b === "string") {
+      let localVariable = {};
+      localVariable[a] = a;
+      localVariable[b] = b;
+      return localVariable;
+    } else {
+      console.error("문자열로 바꿔줘");
+    }
+  } else {
+    console.error("문자열로 바꿔줘");
+  }
+}
 
-let test = workone("유으뜸","euddeum");
+let test = workone(1, 2);
 console.log(test);
-
-
 
 // // * 변수 하나를 객체로 만드는것
 
@@ -23,4 +42,3 @@ console.log(test);
 
 // obj[a] = a;
 // console.log(obj);
-
